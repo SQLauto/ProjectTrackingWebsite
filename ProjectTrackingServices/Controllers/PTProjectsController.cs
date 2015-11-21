ff259ab30a44a283170210855f1d9c7975875b9c
Taramouse <taramouse@live.co.uk>
2015-11-21 20:11:23 +0000

    Added PTProjectsController. Then it's repository. Added PTUserStory controller and repository. Added PTUserTasks controller and repository. Added PTManagerComments controller and repository.

---
+++ ProjectTrackingServices/Controllers/PTProjectsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProjectTrackingServices.Controllers
+{
+    public class PTProjectsController : ApiController
+    {
+        // GET: api/PTProjects
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/PTProjects/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PTProjects
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/PTProjects/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/PTProjects/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
