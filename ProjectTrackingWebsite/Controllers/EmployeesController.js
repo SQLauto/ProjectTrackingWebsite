54ac6189663b26cc54f342e4b3afdc65c449250c
Taramouse <taramouse@live.co.uk>
2015-11-22 16:55:26 +0000

    Edited Home.html. Included HomeController.js script reference. Added ng-controller reference to body tag. Added CompanyName binding. Removed referencing woff type  fonts by editing web.config in both projects.

---
+++ ProjectTrackingWebsite/Controllers/EmployeesController.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    var EmployeesController = function ($scope) {
+        var employees = {
+            employeeName: "John Richard",
+            designation: "Project Manager",
+            contactNo: "+333 3888389",
+            eMailID: "john@projects.com",
+            skillSets: "ASP.NET, ASP.NET MVC"
+        };
+        $scope.Title = "Employee Details Page";
+        $scope.Employees = employees;
+    };
+    app.controller("EmployeesController", EmployeesController);
+}());
