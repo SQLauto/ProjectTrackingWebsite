0e3334918000cc247f3ddd47f5e22dcc9f275b1e
Taramouse <taramouse@live.co.uk>
2015-11-22 21:57:20 +0000

    Added employee search.

--- ProjectTrackingWebsite/Controllers/EmployeesController.js
+++ ProjectTrackingWebsite/Controllers/EmployeesController.js
@@ -2,6 +2,11 @@
     var EmployeesController = function ($scope, $http) {
         var employees = function (serviceResp) {
             $scope.Employees = serviceResp.data;
+
+        };
+        $scope.SearchEmployee = function (EmployeeName) {
+            $http.get("http://localhost:2464/api/ptemployees/" + EmployeeName)
+            .then(employees, errorDetails);
         };
         var errorDetails = function (serviceResp) {
             $scope.Error = "Something went wrong ??";
@@ -9,6 +14,7 @@
         $http.get("http://localhost:2464/api/ptemployees")
             .then(employees, errorDetails);
         $scope.Title = "Employee Details Page";
+        $scope.EmployeeName = null;
     };
     app.controller("EmployeesController", EmployeesController);
 }());
