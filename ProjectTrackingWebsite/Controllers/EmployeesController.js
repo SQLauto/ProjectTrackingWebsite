3cae7bfce6312d56c891b1b7f31994e3db16efc6
Taramouse <taramouse@live.co.uk>
2015-11-23 16:56:44 +0000

    Add logging service. Added $log to EmployeesController.js searchEmployee function.

--- ProjectTrackingWebsite/Controllers/EmployeesController.js
+++ ProjectTrackingWebsite/Controllers/EmployeesController.js
@@ -1,5 +1,5 @@
 ﻿(function () {
-    var EmployeesController = function ($scope, $http) {
+    var EmployeesController = function ($scope, $http, $log) {
         var employees = function (serviceResp) {
             $scope.Employees = serviceResp.data;
 
@@ -7,6 +7,7 @@
         $scope.SearchEmployee = function (EmployeeName) {
             $http.get("http://localhost:2464/api/ptemployees/" + EmployeeName)
             .then(employees, errorDetails);
+            $log.info('Found Employee which contains - ' + EmployeeName);
         };
         var errorDetails = function (serviceResp) {
             $scope.Error = "Something went wrong ??";
